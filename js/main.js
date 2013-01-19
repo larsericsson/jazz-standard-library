f3800b8934af1b9c948a562f34b135d3eaea7c03
John Häggkvist <john.haggkvist@gmail.com>
2013-01-19 18:11:10 +0100

    Change limit

--- js/main.js
+++ js/main.js
@@ -175,7 +175,7 @@ window.onload = function() {
                         +'&bucket=id:spotify-WW&bucket=tracks&bucket=audio_summary',
                     params = {
                         format: 'json',
-                        results: 3,
+                        results: 200,
                         limit: 'true'
                     };
 
